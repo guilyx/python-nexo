069ee579739ac1c700540a44a66f9e2ed456b421
Erwin Lejeune <erwin.lejeune@cm-robotics.com>
2022-10-04 00:27:02 +0200

    add some tests

--- examples/get_account_balances.py
+++ examples/get_account_balances.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 key = os.getenv("NEXO_PUBLIC_KEY")
-secret = os.getenv("NEXO_PUBLIC_KEY")
+secret = os.getenv("NEXO_SECRET_KEY")
 
 client = nexo.Client(key, secret)
 balances = client.get_account_balances()
---
+++ tests/test_serialized_responses.py
@@ -0,0 +1,95 @@
+import sys
+from os import path
+
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+from nexo.response_serializers import *
+import pytest
+
+def test_balances():
+    balances_json = {
+        'balances': [
+            {
+                'assetName': 'BTC', 
+                'totalBalance': '0.00000000', 
+                'availableBalance': '0.00000000', 
+                'lockedBalance': '0.00000000', 
+                'debt': '0.00000000', 
+                'interest': '0.00000000'
+            }
+        ]
+    }
+
+    balances = Balances(balances_json)
+
+    assert(len(balances.balances) == 1)
+    assert(balances.balances[0].asset_name == "BTC")
+    assert(balances.balances[0].total_balance == "0.00000000")
+    assert(balances.balances[0].available_balance == "0.00000000")
+    assert(balances.balances[0].locked_balance == "0.00000000")
+    assert(balances.balances[0].interest == "0.00000000")
+    assert(balances.balances[0].debt == "0.00000000")
+
+    balances_json = {
+        'balances': [
+            {
+                'assetName': 'XRP', 
+                'totalBalance': '100.0',
+                'availableBalance': '2.0',
+                'lockedBalance': '0.3',
+                'interest': '0.4'
+            }
+        ]
+    }
+
+    balances = Balances(balances_json)
+
+    assert(len(balances.balances) == 1)
+    assert(balances.balances[0].asset_name == "XRP")
+    assert(balances.balances[0].total_balance == "100.0")
+    assert(balances.balances[0].available_balance == "2.0")
+    assert(balances.balances[0].locked_balance == "0.3")
+    assert(balances.balances[0].interest == "0.4")
+
+    with pytest.raises(AttributeError):
+        assert(balances.balances[0].debt == "3.0")
+
+def test_pairs():
+    pairs_json = {
+        'pairs': ['BNB/USDT', 'MKR/BTC'],
+        'minLimits': {
+            'BNB/USDT': 0.355,
+            'MKR_BTC': 0.002
+        },
+        'maxLimits': {
+            'BNB/USDT': 3435.5,
+            'MKR_BTC': 42.4
+        }
+    }
+
+    pairs = Pairs(pairs_json)
+    assert(len(pairs.pairs) == 2)
+    assert(pairs.pairs[0] == 'BNB/USDT')
+    assert(pairs.pairs[1] == 'MKR/BTC')
+    assert(pairs.min_limits['BNB/USDT'] == 0.355)
+
+    pairs_json = {
+        'pairs': ['BNB/USDT', 'MKR/BTC'],
+        'min_limits': {
+            'BNB/USDT': 0.355,
+            'MKR_BTC': 0.002
+        },
+        'max_limits': {
+            'BNB/USDT': 3435.5,
+            'MKR_BTC': 42.4
+        }
+    }
+
+    pairs = Pairs(pairs_json)
+
+
+    with pytest.raises(AttributeError):
+        assert(pairs.min_limits == {'BNB/USDT': 0.355, 'MKR_BTC': 0.002})
+    
+    with pytest.raises(AttributeError):
+        assert(pairs.max_limits == {'BNB/USDT': 3435.5, 'MKR_BTC': 42.4})
