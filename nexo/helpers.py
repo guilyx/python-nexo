8be38d0d578f675f2a6cbe0dbdf98c037e3c28fa
Erwin Lejeune <erwin.lejeune@cm-robotics.com>
2022-10-03 15:33:06 +0200

    remove prints

--- nexo/helpers.py
+++ nexo/helpers.py
@@ -7,11 +7,8 @@ def check_pair_validity(pair: str)-> bool:
 
     if len(assets) != 2:
         return False
-
-    print(assets)
     
     for asset in assets:
-        print(asset)
         if not re.match(r"[A-Z]{2,6}", asset) or len(asset) > 6:
             return False
 
