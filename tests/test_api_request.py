09a3646babfcc5148e01ec442d1a80bd3eceb49d
Erwin Lejeune <erwin.lejeune@cm-robotics.com>
2022-10-03 04:58:27 +0200

    test helpers

---
+++ tests/test_api_request.py
@@ -0,0 +1,13 @@
+import sys
+from os import path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+import unittest
+import nexo
+
+class ApiTestFixture(unittest.TestCase):
+    def setUp(self):
+        self.client = nexo.Client("", "")
+    
+    def test_test(self):
+        assert(True)
